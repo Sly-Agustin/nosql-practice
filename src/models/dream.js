02a2f9a8f40f797c4c2ff92e3d31b64bf3f5ced4
Agustin Sly <agustin_sly@hotmail.com>
2022-02-14 16:47:57 -0300

    Added validation to schema

--- src/models/dream.js
+++ src/models/dream.js
@@ -2,48 +2,48 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Dream = new Schema({
-    date: Date,
-    userId: Number,
+    date: {type: Date, required: [true, "date was not provided"]},
+    userId: {type: Number, required: [true, "userId was not provided"]},
     foodAndDrinks: {
         drink: {
-            water: String,
-            sugaryDrink: String,
-            energyDrink: String,
-            caffeineDrink: String,
-            alcohol: String
+            water: {type: String, required: [true, "Water field required"]},
+            sugaryDrink: {type: String, required: [true, "sugaryDrink field required"]},
+            energyDrink: {type: String, required: [true, "energyDrink field required"]},
+            caffeineDrink: {type: String, required: [true, "caffeineDrink field required"]},
+            alcohol: {type: String, required: [true, "alcohol field required"]}
         }
     },
     screenUse: {
         beforeSleep: {
-            mobilePhone: Boolean,
-            television: Boolean,
-            tablet: Boolean,
-            computer: Boolean
+            mobilePhone: {type: Boolean, required: [true, "mobilePohne info required"]},
+            television: {type: Boolean, required: [true, "television info required"]},
+            tablet: {type: Boolean, required: [true, "tablet info required"]},
+            computer: {type: Boolean, required: [true, "computer info required"]}
         },
         throughDay: {
-            socialMedia: Boolean,
-            videogames: Boolean
+            socialMedia: {type: Boolean, required: [true, "socialMedia info required"]},
+            videogames: {type: Boolean, required: [true, "videogames info required"]}
         }
     },
     physicalActivity: {
         training: {
-            timeHours: Number,
-            timeMinutes: Number
+            timeHours: {type: Number, required: [true, "timeHours required"]},
+            timeMinutes: {type: Number, required: [true, "timeMinutes required"]}
         }
     },
     timeManagement: {
         tools: {
-            scheduleBook: Boolean,
-            app: Boolean,
-            notes: Boolean,
-            cellphone: Boolean
+            scheduleBook: {type: Boolean, required: [true, "scheduleBook required"]},
+            app: {type: Boolean, required: [true, "app required"]},
+            notes: {type: Boolean, required: [true, "notes required"]},
+            cellphone: {type: Boolean, required: [true, "cellphone required"]}
         }
     },
     sleepManagement: {
-        quality: String,
+        quality: {type: String, required: [true, "quality required"]},
         sleep: {
-            sleepTime: String,
-            wakeTime: String
+            sleepTime: {type: String, required: [true, "sleepTime required"]},
+            wakeTime: {type: String, required: [true, "wakeTime required"]}
         }
     }
 });

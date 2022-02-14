2ffc741d475f449010a4c3f7913ba0e0a294a972
Agustin Sly <agustin_sly@hotmail.com>
2022-02-14 16:07:36 -0300

    Update healthController.js
    - Now health route succeed if the controller connects with the DB. It also send an error 500 if it fails to connect.

--- src/controller/healthController.js
+++ src/controller/healthController.js
@@ -1,7 +1,11 @@
+const mongoose = require('mongoose');
+
 export const getHealthController = async (req, res) => {
   try {
-    res.status(200).json({ data: "Health connected" });
+    const credentials = process.env.MONGODB_URL;
+    await mongoose.connect(credentials);
+    res.status(200).json({ data: "Health connected to database" });
   } catch (err) {
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: 'Could not connect to database' });
   }
 };

ba611b738b6743ef13b8d427c18157eece0d9aaa
Agustin Sly <agustin_sly@hotmail.com>
2022-03-02 15:16:52 -0300

    Added student validation
    - createStudentController now validates the data it receives to create a new one

--- src/controller/students/createStudentController.js
+++ src/controller/students/createStudentController.js
@@ -1,13 +1,28 @@
 import Student from '../../models/student';
 const mongoose = require('mongoose');
 
+const createStudent = async (studentModel, studentData) => {
+    const student = new studentModel(studentData);
+    let error = student.validateSync();
+    if(error!=undefined) {
+      let errorsMessage = {error: "bad request"}
+      return errorsMessage;
+    }
+    return student;
+  }
+
 export const createStudentController = async (req, res) => {
     try {
         const { body } = req;
 
-        const newStundent = new Student(body)
-        await newStundent.save();
-        res.status(200).json({ message: "Student created succesfully" });
+        const newStundent = await createStudent(Student, body)
+        if(newStundent.error){
+            res.status(400).json(newStundent);
+        }
+        else {
+            await newStundent.save();
+            res.status(200).json({ message: "Student created succesfully" });
+        }
     }
     catch(err){
         res.status(500).json({ message: "Internar error server" })

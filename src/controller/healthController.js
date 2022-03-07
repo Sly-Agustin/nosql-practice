05ca9d525d22267bdf2581cddb38435f45f0388b
Agustin Sly <agustin_sly@hotmail.com>
2022-03-07 14:49:40 -0300

    Update healthController.js

--- src/controller/healthController.js
+++ src/controller/healthController.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 export const getHealthController = async (req, res) => {
-  try {
-    if(mongoose.connection.readyState==1){
-      res.status(200).json({ data: "Health connected to database" });
-    }
-    else{
-      res.status(500).json({ error: "Connection to database was not established"})
-    }
-  } catch (err) {
-    res.status(500).json({ message: 'Could not connect to database' });
+
+  if(mongoose.STATES[mongoose.connection.readyState] == "connected"){
+    res.status(200).json({ data: "Health connected to database" });
+  }
+  else{
+    res.status(500).json({ error: "Connection to database was not established"})
   }
 };
